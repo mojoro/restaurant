5511a18401f55fd07a7f8671a70d5f959b460e88
mojoro <45500240+mojoro@users.noreply.github.com>
2024-04-07 22:00:57 +0200

    Complete webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,13 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
